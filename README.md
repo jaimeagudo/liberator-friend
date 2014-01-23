8a6caa041f0f001f7ef6f7955fe29a82a4a6af27
jaimeagudo <jag@posteo.net>
2014-01-23 23:32:30 +0100

    datomic support included

--- README.md
+++ README.md
@@ -2,9 +2,13 @@
 
 It includes an extra Friend workflow to deal with JSON content on the request body based on Sam's previous [works](https://gist.github.com/sritchie/7552129) as well. Thanks from here Sam!
 
+It also includes awesome Datomic DB, thanks to http://aan.io/datomic-n00biness/
+
 Example code for Sam's [blog post](http://sritchie.github.io/2014/01/17/api-authentication-with-liberator-and-friend/) on using [Liberator](https://github.com/clojure-liberator/liberator) and [Friend](https://github.com/cemerick/friend) to write RESTful APIs with a great authentication and authorization story.
 
 ## License
+
+
 
 Copyright © 2014 PaddleGuru LLC
 
--- project.clj
+++ project.clj
@@ -4,7 +4,18 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main liberator-friend.core
+  :datomic {:schemas ["resources" ["schema.edn" ]]
+            }
+  ; bin/transactor config/samples/free-transactor-template.properties
+
+  :profiles {:dev
+              {:datomic {:config resources/transactor.properties
+                          ; "config/samples/free-transactor-template.properties"
+               :db-uri "datomic:free://127.0.0.1:4334/my-db"}
+               }
+  }
   :dependencies [[org.clojure/clojure "1.5.1"]
+                 [com.datomic/datomic-free "0.9.4470"]
                  [com.cemerick/friend "0.2.0"]
                  [liberator "0.10.0"]
                  [compojure "1.1.5"]
@@ -14,3 +25,5 @@
                  [ring/ring-devel "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  ])
+
+; the library version available at clojars has been upgraded today btw https://clojars.org/com.datomic/datomic-free
